7ca1d511259202c03fb496bc671a31076e87744f
Tiana Oktaviana <tianaoktav@gmail.com>
2022-11-05 09:38:46 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+    echo"Hello Word<br>";
+
+    $nama = "Tiana";
+    $umur = "20";
+
+    echo "Nama saya adalah <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaAdik  = "Via";
+    $umurAdik = "11";
+
+    echo "Nama dik saya adalah <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+    $selisih = $umur - $umurAdik;
+    echo "Selisih usia saya dan adik adalah $selisih tahun";
+
+
+?>
